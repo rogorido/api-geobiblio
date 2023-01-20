0f9f6a7466c20571a20405f2f5d61bc9cfa6a4e0
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2023-01-20 18:15:33 +0100

    Function unaccent is in schema vistas.

--- sql/worksglobal.sql
+++ sql/worksglobal.sql
@@ -1,5 +1,8 @@
 -- select all works with filter on several fields.
 
+--- it is important to use vistas.unaccent, otherwise I get an error
+--- because the user can not access the function which is available in
+--- the vistas schema
 SELECT * 
 FROM vistas.vw_web_works
-WHERE to_tsquery('simple', unaccent($1)) @@ searchterms;
+WHERE to_tsquery('simple', vistas.unaccent($1)) @@ searchterms;
--- sql/workstermscats.sql
+++ sql/workstermscats.sql
@@ -2,7 +2,7 @@
 
 SELECT * 
 FROM vistas.vw_web_works
-WHERE (to_tsquery('simple', unaccent($1)) @@ searchterms)
+WHERE (to_tsquery('simple', vistas.unaccent($1)) @@ searchterms)
       AND work_id IN
       (SELECT work_id FROM works_categories
        WHERE category_id IN ($2:raw));
